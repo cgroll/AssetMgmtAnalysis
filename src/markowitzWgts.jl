4a18b45332fca4d22aca5aabb498a0f0838b2bcf
Christian Groll <groll.christian.edu@gmail.com>
2014-05-23 17:10:01 +0200

    script to evaluate markowitz approaches in for loop

--- src/markowitzWgts.jl
+++ src/markowitzWgts.jl
@@ -2,45 +2,71 @@
 include("/home/chris/research/AssetMgmtAnalysis/src/setup.jl")
 include("/home/chris/.julia/v0.3/AssetMgmt/src/AssetMgmt.jl")
 
-########################
-## specify parameters ##
-########################
-
-## choose starting point to pick subsample, maximum 4607
-cutoff = 1
-
-## choose mu-sigma-estimator and parameters
-muSigmaEstimator = AssetMgmt.empiricalEstimator
-minObs = 400
-rollingWindow = false
 intRatesArr = core(intRates)
 
-## choose response strategy
-responseFunc = AssetMgmt.gmv
-responseFunc = AssetMgmt.maxSharpeRatio
+responses = {"gmv" => AssetMgmt.gmv,
+             "maxSR" => AssetMgmt.maxSharpeRatio,
+             "gmvNoSS" => AssetMgmt.gmvNoSS}
+responses = {"gmv" => AssetMgmt.gmv}
 
-t = Task(() -> AssetMgmt.produceMoments(discRet,
-                                        muSigmaEstimator,
-                                        (),
-                                        minObs=minObs,
-                                        rolling=rollingWindow))
+rollingAlternatives = {"RO" => true,
+                       "NO" => false}
 
-############################
-## get associated weights ##
-############################
+estimators = {"emp" => AssetMgmt.empiricalEstimator}
 
-wgts = NaN*ones(size(discRet))
-dates = rep(false, nObs)
-for x in t
-    (mus, covMatr, index) = x
-    println(index)
-    dates[index] = true
-    ## wgts[index, :] = responseFunc(mus, covMatr)
-    wgts[index, :] = responseFunc(mus, covMatr, intRatesArr[index-1])
+minObsAlternatives = [100, 250, 500]
+
+## auxEstimationDataRequired = 
+
+for muSigmaEstimator in estimators
+    
+    for responseFuncVals in responses
+        
+        for rollingWindow in rollingAlternatives
+            
+            for minObs in minObsAlternatives
+
+                ###################################
+                ## set up moments estimator task ##
+                ###################################
+
+                ## choose response strategy
+                t = Task(() -> AssetMgmt.produceMoments(discRet,
+                                                        muSigmaEstimator[2],
+                                                        (),
+                                                        minObs=minObs,
+                                                        rolling=rollingWindow[2]))
+
+                responseFunc = responseFuncVals[2]
+                
+                ############################
+                ## get associated weights ##
+                ############################
+
+                wgts = NaN*ones(size(discRet))
+                dates = rep(false, nObs)
+                for x in t
+                    (mus, covMatr, index) = x
+                    println(index)
+                    dates[index] = true
+                    wgts[index, :] = responseFunc(mus, covMatr)
+                    ## wgts[index, :] = responseFunc(mus, covMatr, intRatesArr[index-1])
+                end
+
+                wgtsDf = AssetMgmt.composeDataFrame(wgts[dates, :], names(discRet))
+                invs = AssetMgmt.Investments(wgtsDf, idx(discRet)[dates])
+
+                ## save weights to disc
+                wgtsName = string("data/",
+                                  muSigmaEstimator[1], "_",
+                                  responseFuncVals[1], "_",
+                                  rollingWindow[1], "_",
+                                  minObs, ".csv")
+
+                print(wgtsName, "\n")
+                                  
+                AssetMgmt.writeInvestments(wgtsName, invs)
+            end
+        end
+    end
 end
-
-wgtsDf = AssetMgmt.composeDataFrame(wgts[dates, :], names(discRet))
-invs = AssetMgmt.Investments(wgtsDf, idx(discRet)[dates])
-
-## save weights to disc
-AssetMgmt.writeInvestments("data/emp_maxSrWgts.csv", invs)
