35b8cb1b7842c1ae02f8fc1a0b9872815a9e9c0c
cgroll <groll.christian.edu@gmail.com>
2016-02-03 17:17:03 +0100

    first kind of solution

--- mu_sigma_graphics/assembly_code.js
+++ mu_sigma_graphics/assembly_code.js
@@ -1,3 +1,7 @@
+// define accessor functions
+function xAcc(d) { return +d.annual_sigmas; }
+function yAcc(d) { return +d.annual_mus; }
+
 // define margins
 var margin = {top: 20, right: 80, bottom: 30, left: 150};
 
@@ -12,18 +16,18 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // axes scales
-var x = d3.scale.linear()
+var xScale = d3.scale.linear()
     .range([0, width]);
 
-var y = d3.scale.linear()
+var yScale = d3.scale.linear()
     .range([height, 0]);
 
 var xAxis = d3.svg.axis()
-    .scale(x)
+    .scale(xScale)
     .orient("bottom");
 
 var yAxis = d3.svg.axis()
-    .scale(y)
+    .scale(yScale)
     .orient("left")
     .ticks(5);
 
@@ -32,17 +36,23 @@ var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 var line = d3.svg.line()
     .interpolate("basis")
-    .x(function(d) { return x(d.annual_sigmas); })
-    .y(function(d) { return y(d.annual_mus); });
+    .x(function(d) { return xAcc(d); })
+    .y(function(d) { return yAcc(d); });
 
 var momentData = d3.csv("allUnivInfo_short.csv", function (data) {
+	 
+	 // // transform dates to date format
+    // data.forEach(function(d) {
+    //     d.idx = parseDate(d.idx);
+    // });
+	 
+	 // group data by date
+	 var groupedDates = d3.nest()
+		  .key(function(d) { return d.idx; })
+		  .entries(data);
     
-    data.forEach(function(d) {
-        d.idx = parseDate(d.idx);
-    });
-    
-    x.domain(d3.extent(data, function(d) { return +d.annual_sigmas; }));
-	 y.domain(d3.extent(data, function(d) { return +d.annual_mus; }));
+    xScale.domain(d3.extent(data, function(d) { return xAcc(d); }));
+	 yScale.domain(d3.extent(data, function(d) { return yAcc(d); }));
     
     svg.append("g")
         .attr("class", "x axis")
@@ -57,58 +67,99 @@ var momentData = d3.csv("allUnivInfo_short.csv", function (data) {
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("GDP in bn $");
-    
-    // var gdp = svg.selectAll("circle")
-    //     .data(data)
-    //     .enter()
-    //     .append("circle")
-	 // 	  .attr("cx", function(d) {
-	 // 			return x(+d.annual_sigmas);
-	 // 	  })
-	 // 	  .attr("cy", function(d) {
-	 // 			return y(+d.annual_mus);
-	 // 	  })
-	 // 	  .attr("r", 2);
+        .text("Annualized expected return");
 	 
-	 // get current date
-	 var currDate = data[5].idx;
+	 // Add an x-axis label.
+	 svg.append("text")
+		  .attr("class", "x label")
+		  .attr("text-anchor", "end")
+		  .attr("x", width)
+		  .attr("y", height - 6)
+		  .text("Annualized volatility");
 	 
-	 // select all points of current date
-	 svg.selectAll("circle")  
-        .data(data)                                     
-		  .enter().append("circle")
-		  .attr("cx", function(d) {
-	 			return x(+d.annual_sigmas);
-	 	  })
-	 	  .attr("cy", function(d) {
-	 			return y(+d.annual_mus);
-	 	  })                               
-		  .filter(function(d) { return d.idx.getTime() == currDate.getTime() })        // <== This line
-        .style("fill", "red")                            // <== and this one
-        .attr("r", 3.5);
-
-	 var effFront = svg.selectAll("effLine")
-
-	 effFront.datum(data)
-		  .append("g")
-        .attr("class", "effLine")
-        .append("path")
-        .attr("class", "line")
-	 	  .attr("fill", "none")
-	 	  .attr('stroke', 'blue')
-	 	  .attr('stroke-width', 2)
-        .attr("d", line(data));
-
+	 // Add the year label; the value is set on transition.
+	 var label = svg.append("text")
+		  .attr("class", "year label")
+		  .attr("text-anchor", "end")
+		  .attr("y", height - 24)
+		  .attr("x", width)
+		  .text(groupedDates[0].key);
 	 
-	 // svg.append("g")
-    //     .attr("class", "effLine")
-    //     .append("path")
-    //     .attr("class", "line")
-	 // 	  .attr("fill", "none")
-	 // 	  .attr('stroke', 'blue')
-	 // 	  .attr('stroke-width', 2)
-    //     .attr("d", line(data));
-    
+	 // Positions the dots based on data.
+	 function position(dot) {
+	 	  dot.attr("cx", function(d) { return xScale(xAcc(d)); })
+	 			.attr("cy", function(d) { return yScale(yAcc(d)); })
+	 	  		.style("fill", "red")                            // <== and this one
+	 			.attr("r", 3.5);
+	 }
 	 
+	 function update(newData) {
+		  
+		  var momentDots = svg.selectAll("circle")
+				.data(newData);
+		  
+		  // enter method
+		  momentDots.enter()
+				.append("circle")
+				.call(position);
+		  
+		  // update method
+		  momentDots.transition().duration(1000).call(position);
+		  
+		  // exit method
+		  momentDots.exit().remove();
+		  
+	 }
+	 
+	 // var momentDots = update(groupedDates[0].values);
+	 
+	 // Start a transition that interpolates the data based on year.
+	 // momentDots.transition()
+	 //     .duration()
+	 // 	  .data(groupedDates[1].values)
+	 // 	  .call(position);
+	 
+	 // Tweens the entire chart by first tweening the year, and then the data.
+	 // For the interpolated data, the dots and label are redrawn.
+	 // function tweenDate() {
+	 // 	  var thisDateInd = d3.interpolateNumber(0, 2);
+	 // 	  return function(t) { displayDate(groupedDates[t].values); };
+	 // }
+	 
+	 // function displayDate(thisData) {
+	 // 	  momentDots.data(thisData, key).call(position);
+	 // 	  label.text(thisData.key);
+	 // }
+	 
+	 
+	 // // get data for current date
+	 // var currDateData = groupedDates[0].values;
+	 
+	 for ( var i = 0; i <= 2; ++i ) {
+		  // update(groupedDates[i].values);
+		  // update(groupedDates[i].values );
+		  setTimeout( function (){ update(groupedDates[i].values ) }, 2000);
+	 }
+	 // setInterval(function () {
+	 // dateInd = dateInd + 1;
+	 // update(groupedDates[dateInd].values);
+	 // }, 2000)
 })
+// render points subsequently
+// 
+// setTimeout( function (){ render(groupedDates[1].values ); }, 2000);
+// setTimeout( function (){ render(groupedDates[2].values ); }, 3000);
+
+
+// var effFront = svg.selectAll("effLine")
+
+// effFront.datum(data)
+// 	  .append("g")
+//     .attr("class", "effLine")
+//     .append("path")
+//     .attr("class", "line")
+// 	  .attr("fill", "none")
+// 	  .attr('stroke', 'blue')
+// 	  .attr('stroke-width', 2)
+//     .attr("d", line(data));
+
